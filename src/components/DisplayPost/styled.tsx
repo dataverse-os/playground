486f70e5bc41a12bf4e57a147888859a7a332386
moe-moe-pupil <dhtmitmi@gmail.com>
2023-03-14 14:25:16 +0800

    feat: update Text style

--- src/components/DisplayPost/styled.tsx
+++ src/components/DisplayPost/styled.tsx
@@ -15,6 +15,13 @@ export const TextWrapper = styled.div`
   width: 100%;
   height: 100%;
   overflow-wrap: break-word;
+  font-family: Poppins-SemiBold;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 0.96rem;
+  line-height: 1.85rem;
+  margin-left: 0.57rem;
+  color: #000000;
 `;
 
 export const ImgWrapper = styled.div`
